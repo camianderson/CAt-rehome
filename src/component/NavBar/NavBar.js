aff873cfad4fd6a9da5a67e3a0f9fd417743bb70
Cami Anderson <camidantas@icloud.com>
2022-08-03 09:53:40 -0400

    Build out NavBar component

--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <h1>CAt Rehome</h1>
+    <nav>
+      <div className='nav-buttons'>
+        <button className='home-button'>Home</button>
+        <button className='about-button'>About</button>
+      </div>
+      <h1>CAt Rehome</h1>
+      <button className='fav-button'>Favorites</button>
+    </nav>
   )
 }
 
