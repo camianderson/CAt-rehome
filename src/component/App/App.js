819c3cbd6f8aebdc0a202fc563697e1984d9ca1f
Cami Anderson <camidantas@icloud.com>
2022-08-03 19:33:28 -0400

    Add Link to Card

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -4,7 +4,7 @@ import CatDescription from '../CatDescription/CatDescription';
 import Favorites from '../Favorites/Favorites';
 import List from '../List/List';
 import NavBar from '../NavBar/NavBar';
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, Switch} from 'react-router-dom';
 import './App.css';
 
 const App = () => {
@@ -34,18 +34,20 @@ const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <Route exact path='/' render={() => (
-        <section className='main-page'>
-          <img className='main-img' src='https://wallup.net/wp-content/uploads/2018/09/25/602884-baby_animals-kittens-cat.jpg' />
-          <Link to='/list'>
-            <button className='button-adopt'>Adopt a Cat!</button>
-          </Link>
-        </section>
-      )}/>
-      <Route path='/about' render={() => <About />}/>
-      <Route path='/favorites' render={() => <Favorites />}/>
-      <Route path='/list' render={() => <List cats={catsData} selectCat={selectCat}/>}/>
-      <Route path='/cat-description' render={() => <CatDescription cat={selectedCat}/>}/>
+      <Switch>
+        <Route exact path='/' render={() => (
+          <section className='main-page'>
+            <img className='main-img' src='https://wallup.net/wp-content/uploads/2018/09/25/602884-baby_animals-kittens-cat.jpg' />
+            <Link to='/list'>
+              <button className='button-adopt'>Adopt a Cat!</button>
+            </Link>
+          </section>
+        )}/>
+        <Route path='/about' render={() => <About />}/>
+        <Route path='/favorites' render={() => <Favorites />}/>
+        <Route path='/list' render={() => <List cats={catsData} selectCat={selectCat}/>}/>
+        <Route path='/cat-description/:id' render={() => <CatDescription cat={selectedCat}/>}/>
+      </Switch>
     </div>
   );
 }
--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import './Card.css'
 
 const Card = ({name, picture, id, selectCat}) => {
   return (
-    <section className='cat-info' onClick={() => selectCat(id)}>
-      <img src={picture}/>
-      <h3>{name}</h3>  
-    </section>
+    <Link to={`/cat-description/${id}`} style={{ textDecoration: "none" }}>
+      <section className='cat-info' onClick={() => selectCat(id)}>
+        <img src={picture}/>
+        <h3>{name}</h3>  
+      </section>
+    </Link>
   )
 }
 
