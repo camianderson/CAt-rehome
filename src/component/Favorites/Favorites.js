c8318d479af5348f700d9fa9ebfeea348c0bf0b3
Cami Anderson <camidantas@icloud.com>
2022-08-05 15:14:47 -0400

    Add style to the favorites page

--- src/component/Favorites/Favorites.js
+++ src/component/Favorites/Favorites.js
@@ -8,9 +8,13 @@ const Favorites = ({favCats, selectCat}) => {
     return (
       <Link to={`/cat-description/${cat.id}`} style={{ textDecoration: "none" }} key={cat.id}>
         <div className='fav-cat-info' onClick={() => selectCat(cat.id)}>
-          <img src={cat.picture}/>
-          <h3>{cat.name}</h3>
-          <p>{cat.sex} | {cat.type} | {cat.size}</p> 
+          <div className='border-fav-img'>
+            <img className='fav-img' src={cat.picture}/>
+          </div>
+          <div className='name-fav'>
+            <h3>{cat.name}</h3>
+            <p>{cat.sex} | {cat.type} | {cat.size}</p> 
+          </div>
         </div>
       </Link>
     )
